fa286073742f2958edf7ba1ab9104b0c223cf880
pankajnaik <115211948+pankaj297@users.noreply.github.com>
2024-04-07 10:10:41 +0530

    connect render

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
-const DBURL = "mongodb://0.0.0.0:27017";
+const DBURL =
+  "mongodb+srv://pankajnaik958:test123@cluster2.u3axap5.mongodb.net/test";
 
 mongoose
   .connect(DBURL, {})
